ae5dfd923f3e872881c95bf030fbac2ded1c0068
Allan Panchi <avpanchi@espe.edu.ec>
2023-06-19 12:31:30 -0500

    CRUD Contactos

--- main.py
+++ main.py
@@ -1,84 +1,76 @@
-class Contactos:
-    def __init__(self, nombre, apellido, numero, edad):
+class Pelicula:
+    def __init__(self, nombre, anio):
         self.nombre = nombre
-        self.apellido = apellido
-        self.numero = numero
-        self.edad = edad
+        self.anio = anio
 
     def __str__(self):
-        return f"Nombre: {self.nombre}, Apellido: {self.apellido}, Numero: {self.numero}, Edad: {self.edad}"
+        return f"{self.nombre} ({self.anio})"
 
 
-def mostrar_contactos(contactos):
-    if contactos:
-        print("Contactos ingresados ingresadas:")
-        for contacto in contactos:
-            print(contacto)
+def mostrar_peliculas(peliculas):
+    if peliculas:
+        print("Películas ingresadas:")
+        for pelicula in peliculas:
+            print(pelicula)
     else:
-        print("No se han ingresado contactos.")
+        print("No se han ingresado películas.")
 
 
-def eliminar_contacto(contactos):
-    if contactos:
-        nombre = input("Ingrese el nombre del contacto a eliminar: ")
-        contactos_filtrados = [contacto for contacto in contactos if contacto.nombre != nombre]
-        if len(contactos_filtrados) < len(contactos):
-            contactos[:] = contactos_filtrados
-            print("Contacto eliminada exitosamente.")
+def eliminar_pelicula(peliculas):
+    if peliculas:
+        nombre = input("Ingrese el nombre de la película a eliminar: ")
+        peliculas_filtradas = [pelicula for pelicula in peliculas if pelicula.nombre != nombre]
+        if len(peliculas_filtradas) < len(peliculas):
+            peliculas[:] = peliculas_filtradas
+            print("Película eliminada exitosamente.")
         else:
-            print("No se encontró a un contacto con ese nombre.")
+            print("No se encontró una película con ese nombre.")
     else:
-        print("No se han ingresado contactos.")
+        print("No se han ingresado películas.")
 
 
-def modificar_contacto(contactos):
-    if contactos:
-        nombre = input("Ingrese el nombre del contacto a modificar: ")
-        for contacto in contactos:
-            if contacto.nombre == nombre:
-                nuevo_nombre = input("Ingrese el nuevo nombre del contacto: ")
-                nuevo_apellido = input("Ingrese el nuevo apellido del contacto: ")
-                nuevo_numero = int(input("Ingrese el nuevo numero del contacto: "))
-                nuevo_edad = int(input("Ingrese la nueva edad del contacto: "))
-                contacto.nombre = nuevo_nombre
-                contacto.apellido = nuevo_apellido
-                contacto.numero = nuevo_numero
-                contacto.edad = nuevo_edad
-                print("Contacto modificada exitosamente.")
+
+def modificar_pelicula(peliculas):
+    if peliculas:
+        nombre = input("Ingrese el nombre de la película a modificar: ")
+        for pelicula in peliculas:
+            if pelicula.nombre == nombre:
+                nuevo_nombre = input("Ingrese el nuevo nombre de la película: ")
+                nuevo_anio = int(input("Ingrese el nuevo año de la película: "))
+                pelicula.nombre = nuevo_nombre
+                pelicula.anio = nuevo_anio
+                print("Película modificada exitosamente.")
                 return
-        print("No se encontró a un contacto con ese nombre.")
+        print("No se encontró una película con ese nombre.")
     else:
-        print("No se han ingresado contactos.")
+        print("No se han ingresado películas.")
 
 def menu():
-    contactos = []
+    peliculas = []
 
     while True:
         print("------ MENÚ ------")
-        print("1. Ingresar contacto")
-        print("2. Mostrar contactos")
-        print("3. Eliminar contacto")
-        print("4. Modificar contacto")
+        print("1. Ingresar película")
+        print("2. Mostrar películas")
+        print("3. Eliminar película")
+        print("4. Modificar película")
         print("0. Salir")
 
-        opcion = int(input("Ingrese una opción: "))
+        opcion = input("Ingrese una opción: ")
 
-        if opcion == 2:
-            nombre = input("Ingrese el nombre de la persona: ")
-            apellido = input("Ingrese el apellido de la persona: ")
-            numero = int(input("Ingrese el numero de la persona: "))
-            edad = int(input("Ingrese la edad de la persona: "))
-            
-            contacto = Contactos(nombre, apellido, numero, edad)
-            contactos.append(contacto)
-            print("Contacto ingresada exitosamente.")
-        elif opcion == 2:
-            mostrar_contactos(contactos)
-        elif opcion == 3:
-            eliminar_contacto(contactos)
-        elif opcion == 4:
-            modificar_contacto(contactos)
-        elif opcion == 0:
+        if opcion == "1":
+            nombre = input("Ingrese el nombre de la película: ")
+            anio = int(input("Ingrese el año de la película: "))
+            pelicula = Pelicula(nombre, anio)
+            peliculas.append(pelicula)
+            print("Película ingresada exitosamente.")
+        elif opcion == "2":
+            mostrar_peliculas(peliculas)
+        elif opcion == "3":
+            eliminar_pelicula(peliculas)
+        elif opcion == "4":
+            modificar_pelicula(peliculas)
+        elif opcion == "0":
             print("¡Hasta luego!")
             break
         else:
